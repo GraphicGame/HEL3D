4bdf479b87661ec0ef5d911b83a0955fd99740c0
Graphics <softgiant@126.com>
2015-03-15 01:29:12 +0800

    color_buffer test case.

--- HEL3D/HEL3D_TestCases/main.cpp
+++ HEL3D/HEL3D_TestCases/main.cpp
@@ -1,27 +1,45 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-#include "device.h" 
+#include "device.h"
+#include "image.h"
 
 const int WW = 960;
 const int WH = 640;
 
-color ClearColor = {0, 255, 255, 255};
-color_buffer *CB = NULL;
+color ClearColor = {0, 0, 0, 255};
+color_buffer *CB = nullptr;
+image_data *img_data = nullptr;
 
 static void on_draw() {
 	CB->clear(ClearColor);
+
+	CB->write_pixels(img_data, 0, 0);
+
 	for (uint row = 0; row < 100; row++) {
 		for (uint col = 0; col < 100; col++) {
-			CB->write_color(col, row, color(255, 255, 0, 255));
+			if (row <= 50)
+				CB->write_color(col, row, color(255, 0, 0, 255));
+			else
+				CB->write_color(col, row, color(0, 0, 255, 255));
 		}
 	}
 	CB->flush();
 }
 
+static void load_img(const char *path) {
+	if (img_data == nullptr) {
+		img_data = new image_data();
+	}
+	img_decode_image(path, img_data);
+}
+
 int main(int argc, char *argv[]) {
 	CB = device_create_colorbuffer(WW, WH);
-	device_create_window(WW, WH);
+	device_create_window(WW + 30, WH + 30);
+
+	load_img("E:/test.jpg");
+
 	device_register_draw_func(on_draw);
 	device_main_loop();
 
